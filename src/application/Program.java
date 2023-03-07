46f9977fe05a13344b9a38ca7c6098e10004865e
Guilherme Pessoa <gui.senai@live.com>
2023-03-07 14:38:29 -0300

    findById implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
@@ -14,5 +15,9 @@ public class Program {
 		
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		Seller seller = sellerDao.findById(3);
+		
+		System.out.println(seller);
 	}
 }
